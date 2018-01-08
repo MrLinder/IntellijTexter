c3d0b9752faa9aafd102bcf70fae72bb7612b9d8
Lindx <linder.igor@gmail.com>
2018-01-09 00:03:55 +0300

    7

---
+++ 0007 seven/src/cat.java
@@ -0,0 +1,11 @@
+/**
+ * Created by LindXdeep on 08.01.2018.
+ */
+class cat extends animal{
+    @Override
+    void voice()
+    {
+        super.voice();
+        System.out.print("мяу");
+    }
+}
---
+++ 0007 seven/src/mainclass.java
@@ -0,0 +1,14 @@
+/**
+ * Created by LindXdeep on 08.01.2018.
+ */
+public class mainclass {
+    public static void main(String args[])
+    {
+        animal c1 =  new animal();
+        cat a1 = new cat();
+        c1.voice();
+        a1.voice();
+    }
+}
+
+
